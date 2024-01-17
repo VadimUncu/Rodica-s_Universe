6e6f963ed626808bf62d5bd18a92f0d8dfaf6030
Vadim Uncu <uncu.vadim@yahoo.com>
2024-01-17 09:44:34 -0500

    update

--- home.js
+++ home.js
@@ -27,3 +27,7 @@ showSlide(currentIndex);
 // Set up event listeners for next and previous buttons
 document.getElementById('nextBtn').addEventListener('click', nextSlide);
 document.getElementById('prevBtn').addEventListener('click', previousSlide);
+
+function focusInput() {
+  document.getElementById('searchInput').focus();
+}
