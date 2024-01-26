87fdb1309ca22f9a378fc961f217ec44de72a513
Vadim Uncu <uncu.vadim@yahoo.com>
2024-01-26 12:00:00 -0500

    update

--- home.js
+++ home.js
@@ -87,3 +87,10 @@ function myFunction() {
 window.onscroll = function() {
     myFunction();
 };
+
+var burgerMenu = document.getElementById('burger-menu');
+var overlay = document.getElementById('menu');
+burgerMenu.addEventListener('click',function(){
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
